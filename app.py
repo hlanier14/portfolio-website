a99dc9fa18ab460bf50f3911d450a0dec71cfbb8
hlanier14 <60249654+hlanier14@users.noreply.github.com>
2022-05-08 18:19:16 -0500

    created base templates for site structure

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+def home():
+    return render_template("home.html")
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
