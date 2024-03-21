2a941ff550f70e146ee8fffb4c436f5e87b0d213
Harrison Lanier <harrison.lanier@clickvoyant.com>
2024-03-20 22:17:42 -0500

    add total supply charts

--- src/pages/Lending.js
+++ src/pages/Lending.js
@@ -20,23 +20,26 @@ function Lending() {
     
     const [selectedNetwork, setSelectedNetwork] = useState('');
     const [rateData, setRateData] = useState([]);
+    const [supplyData, setSupplyData] = useState([]);
 
     const [loading, setLoading] = useState(true);
 
-    // 
     useEffect(() => {
-      const fetchData = async () => {
-        const response = await axios("https://lending-m7dl7jaevq-uc.a.run.app/protocols/daily");
-        const data = response.data;
-        setProtocolData(data);
-        const networks = [...new Set(data.map(entry => entry.network))];
-        setUniqueNetworks(networks);
-        const protocols = [...new Set(data.map(entry => entry.protocol))];
-        setUniqueProtocols(protocols);
-        setSelectedNetwork('polygon-mainnet')
-        setLoading(false);
-      };
-      fetchData();
+    
+        const fetchData = async () => {
+            const baseUrl = 'https://lending-m7dl7jaevq-uc.a.run.app'
+            // const baseUrl = 'http://localhost:3030'
+            const response = await axios(`${baseUrl}/protocols/daily`);
+            const data = response.data;
+            setProtocolData(data);
+            const networks = [...new Set(data.map(entry => entry.network))];
+            setUniqueNetworks(networks);
+            const protocols = [...new Set(data.map(entry => entry.protocol))];
+            setUniqueProtocols(protocols);
+            setSelectedNetwork('polygon-mainnet')
+            setLoading(false);
+        };
+        fetchData();
     }, []);
 
     useEffect(() => {
@@ -46,62 +49,83 @@ function Lending() {
             "aave": "solid"
         }
         const rates = [];
+        const supply = [];
         uniqueProtocols.forEach(protocol => {
             const protocolDataFiltered = networkData.filter(entry => entry.protocol === protocol);
             protocolDataFiltered.forEach(entry => {
-                rates.push({
+                const color = getRandomColor();
+                const design = {
                     x: entry.data.map(point => point.date),
-                    y: entry.data.map(point => point.supplyAPR),
                     type: 'scatter',
                     mode: 'lines',
                     name: `${entry.asset}`,
                     marker: { 
-                        color: getRandomColor() 
+                        color: color
                     },
                     line: {
                         dash: marker[protocol]
                     }
+                }
+                rates.push({
+                    y: entry.data.map(point => point.supplyAPR),
+                    ...design
+                });
+                supply.push({
+                    y: entry.data.map(point => point.totalSupply),
+                    ...design
                 });
             });
         });
         setRateData(rates)
+        setSupplyData(supply)
     }, [selectedNetwork]);
 
-    const rateLayout = {
-        title: `${selectedNetwork}`,
-        yaxis: { title: 'Supply APR (%)' },
-    };
-
-    const marker = {
-        "compound": "dash",
-        "aave": "solid"
-    }
     const color = {
         "polygon-mainnet": "red",
         "optimism-mainnet": "blue",
         "arbitrum-mainnet": "orange",
         "avalanche-mainnet": "green"
     }
-    const overlayData = [];
-    protocolData.forEach(entry => {
-        overlayData.push({
-            x: entry.data.map(point => point.date),
-            y: entry.data.map(point => point.supplyAPR),
+    const avgRateData = [];
+    const totalSupplyData = [];
+    uniqueNetworks.forEach(network => {
+        const networkData = protocolData.filter(entry => entry.network === network);
+        const maxDataLength = Math.max(...networkData.map(entry => entry.data.length));
+        const avgRates = Array.from({ length: maxDataLength }, (_, index) => {
+            let weightedSum = 0;
+            let totalSupplySum = 0;
+            networkData.forEach(entry => {
+                if (entry.data[index]) {
+                    const weight = entry.data[index].totalSupply / networkData.reduce((acc, curr) => acc + (curr.data[index] ? curr.data[index].totalSupply : 0), 0);
+                    weightedSum += entry.data[index].supplyAPR * weight;
+                    totalSupplySum += weight;
+                }
+            });
+            return totalSupplySum > 0 ? weightedSum / totalSupplySum : 0;
+        });        
+        const totalSupply = Array.from({ length: maxDataLength }, (_, index) => {
+            const values = networkData.map(entry => entry.data[index] ? entry.data[index].totalSupply : 0);
+            return values.reduce((acc, curr) => acc + curr, 0);
+        });
+        const design = {
             type: 'scatter',
             mode: 'lines',
-            name: `${entry.asset}`,
+            name: `${network}`,
             marker: { 
-                color: color[entry.network]
-            },
-            line: {
-                dash: marker[entry.protocol]
+                color: color[network]
             }
+        }
+        avgRateData.push({
+            x: avgRates.map((_, index) => networkData[0].data[index] ? networkData[0].data[index].date : null),
+            y: avgRates,
+            ...design
+        });
+        totalSupplyData.push({
+            x: totalSupply.map((_, index) => networkData[0].data[index] ? networkData[0].data[index].date : null),
+            y: totalSupply,
+            ...design
         });
     });
-    const overlayLayout = {
-        title: 'All Networks',
-        yaxis: { title: 'Supply APR (%)' },
-    };
 
     return (
         <div className="bg-slate-100">
@@ -118,11 +142,48 @@ function Lending() {
                                 <option key={network} value={network}>{network}</option>
                             ))}
                         </select>
-                        <div className='mt-5 mb-24'>
-                            <Plot data={rateData} layout={rateLayout} />
+                        <div className='mt-5 mb-24 space-y-5'>
+                            <div>
+                                <Plot 
+                                    data={rateData} 
+                                    layout={{
+                                        title: 'Supply APR',
+                                        yaxis: { title: '%' },
+                                    }} 
+                                />
+                            </div>
+                            <div>
+                                <Plot 
+                                    data={supplyData} 
+                                    layout={{
+                                        title: 'Total Supply',
+                                        yaxis: { title: '$' },
+                                    }} 
+                                />
+                            </div>
                         </div>
-                        <div className='mt-5 mb-24'>
-                            <Plot data={overlayData} layout={overlayLayout} />
+                        <div className='mb-48 space-y-5'>
+                            <div className='text-center text-xl'>
+                                All Networks
+                            </div>
+                            <div>
+                                <Plot 
+                                    data={avgRateData} 
+                                    layout={{
+                                        title: 'Weighted Average Supply APR',
+                                        yaxis: { title: '%' },
+                                    }} 
+                                />
+                            </div>
+                            <div>
+                                <Plot 
+                                    data={totalSupplyData} 
+                                    layout={{
+                                        title: 'Total Supply',
+                                        yaxis: { title: '$' },
+                                    }}
+                                />
+                            </div>
                         </div>
                     </div>
                 )}
